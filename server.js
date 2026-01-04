fe828941663fad82eb0d491ffb71769bb690608f
V Devanarayan <89205573+CodeTitan7@users.noreply.github.com>
2026-01-04 12:50:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,4 +27,5 @@ const io = new Server(server, { cors: { origin: "*" } });
 
 socketHandler(io);
 
-server.listen(5000, () => console.log("Server running"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
